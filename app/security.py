100ed6b599ad8d1eb6ae1c76c078721627ba1054
dmitrelt <dmitriyeltanskiy48@yandex.ru>
2026-05-14 22:22:02 +0300

    FEAT: Реализована ручка для персональных рекомендаций

--- app/security.py
+++ app/security.py
@@ -7,13 +7,14 @@ async def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security)
 ) -> int:
     """Заглушка авторизации для разработки и лабораторных"""
-    if not credentials or not credentials.credentials:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Не предоставлен токен",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    # if not credentials or not credentials.credentials:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED,
+    #         detail="Не предоставлен токен",
+    #         headers={"WWW-Authenticate": "Bearer"},
+    #     )
     # Для тестов возвращаем фиксированный user_id
-    fake_user_id = 1
-    print(f"[AI Service] Запрос обработан для user_id={fake_user_id}")
-    return fake_user_id
+    # fake_user_id = 1
+    # print(f"[AI Service] Запрос обработан для user_id={fake_user_id}")
+    # return fake_user_id
+    return 1
